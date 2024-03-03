3e0577328dbee013ed17b89b7f4ae0ee464f271b
ArthurNamutilu <arthurggnamutilu@gmail.com>
2024-03-03 17:25:10 +0300

    portfolio

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import About from "./components/About";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Home />
+      <About />
+    </div>
+  );
+}
+
+export default App;
