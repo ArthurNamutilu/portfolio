20447be4d669b740ba719a54b3d337731d2ad26b
ArthurNamutilu <arthurggnamutilu@gmail.com>
2024-03-04 10:01:16 +0300

    work

---
+++ src/components/Work.jsx
@@ -0,0 +1,78 @@
+import WorkImg from "../assets/real-estate.png";
+import GgBlog from "../assets/gg-blog.png";
+
+const Work = () => {
+  return (
+    <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
+      <div className="max-w-[1000px] mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-green-600">
+            Work
+          </p>
+          <p className="py-6">My recent projects</p>
+        </div>
+        {/* container */}
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {/* Grid item */}
+          <div
+            style={{ backgroundImage: `url(${WorkImg})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Real Estate React JS Application
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://globomantics-real-estate-dusky.vercel.app/"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/ArthurNamutilu/GlobomanticsLivingReact"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${GgBlog})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                React JS Mini Blog Application
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://gg-blog-eight.vercel.app/" target="_blank">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/ArthurNamutilu/gg-blog"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Work;
