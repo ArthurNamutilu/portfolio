2fa694c4fd20ece985b2fd64f06aa1c35fa8c724
ArthurNamutilu <arthurggnamutilu@gmail.com>
2024-03-04 19:09:34 +0300

    added cv

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,6 +4,7 @@ import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import { Link } from "react-scroll";
+import cvPdf from "../assets/CV.pdf";
 
 const NavBar = () => {
   const [nav, setNav] = useState(false);
@@ -119,7 +120,9 @@ const NavBar = () => {
             <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
               <a
                 className="flex justify-between items-center w-full text-gray-300 "
-                href="/"
+                href={cvPdf}
+                target="_blank"
+                rel="noopener noreferrer"
               >
                 CV <BsFillPersonLinesFill size={30} />
               </a>
