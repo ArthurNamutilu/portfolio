8969c5e2b824ae039326741f575e9c370e00c300
ArthurNamutilu <arthurggnamutilu@gmail.com>
2024-03-21 01:43:13 +0300

    added shorping cart

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,5 +1,5 @@
 import WorkImg from "../assets/real-estate.png";
-import GgBlog from "../assets/gg-blog.png";
+import ShoppingCart from "../assets/cart.png";
 import Recipe from "../assets/food-recipe.png";
 
 const Work = () => {
@@ -15,6 +15,37 @@ const Work = () => {
         {/* container */}
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           {/* Grid item */}
+          <div
+            style={{ backgroundImage: `url(${ShoppingCart})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover effects */}
+
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                (Redux)-SHOPPING CART
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://shopping-cart-three-eta.vercel.app/"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/ArthurNamutilu/shopping_cart"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* real estste */}
           <div
             style={{ backgroundImage: `url(${Recipe})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
@@ -44,7 +75,6 @@ const Work = () => {
               </div>
             </div>
           </div>
-          {/* real estste */}
           <div
             style={{ backgroundImage: `url(${WorkImg})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
@@ -65,32 +95,6 @@ const Work = () => {
                 </a>
                 <a
                   href="https://github.com/ArthurNamutilu/GlobomanticsLivingReact"
-                  target="_blank"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div
-            style={{ backgroundImage: `url(${GgBlog})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* Hover effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                React JS Mini Blog Application
-              </span>
-              <div className="pt-8 text-center">
-                <a href="https://gg-blog-eight.vercel.app/" target="_blank">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a
-                  href="https://github.com/ArthurNamutilu/gg-blog"
                   target="_blank"
                 >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
